6fd54decc87ef4804a914dc763673ccccfc3339a
chi xu <xuchi007@hotmail.com>
2024-10-06 22:14:32 +1300

    add comments in AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,3 +1,12 @@
+/// <summary>
+/// managing the interaction between your application and the database
+/// </summary>
+/// <remarks>
+/// managing the interaction between your application and the database
+/// </remarks>
+/// <author>
+/// Chi Xu (Peter) -- 06/10/2024
+/// </author>
 using System;
 using Microsoft.EntityFrameworkCore;
 using OXL_Assessment2.Data.Configurations;
@@ -7,20 +16,18 @@ namespace OXL_Assessment2.Data;
 
 public class AppDbContext : DbContext
 {
-  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-  {
-  }
-
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
   public DbSet<Category> Categories { get; set; }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
     base.OnModelCreating(modelBuilder);
-    modelBuilder.ApplyConfiguration(new CategoryConfiguration());
     // all configuration in this assembly
-    // modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+    modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+    // modelBuilder.ApplyConfiguration(new CategoryConfiguration());
   }
 
+  // auto track update and create
   public override int SaveChanges()
   {
     var entries = ChangeTracker
@@ -39,5 +46,25 @@ public class AppDbContext : DbContext
       }
     }
     return base.SaveChanges();
+  }
+  // auto track update and create
+  public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+  {
+    var entries = ChangeTracker
+    .Entries()
+    .Where(e => e.Entity is BaseEntity && (
+        e.State == EntityState.Added
+        || e.State == EntityState.Modified));
+
+    foreach (var entityEntry in entries)
+    {
+      ((BaseEntity)entityEntry.Entity).CreateTime = DateTime.UtcNow;
+
+      if (entityEntry.State == EntityState.Added)
+      {
+        ((BaseEntity)entityEntry.Entity).UpdateTime = DateTime.UtcNow;
+      }
+    }
+    return base.SaveChangesAsync(cancellationToken);
   }
 }
