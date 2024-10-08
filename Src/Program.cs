be3ded22801eb8e40d858f5eb6e03db996351b05
chi xu <xuchi007@hotmail.com>
2024-10-08 20:43:50 +1300

    remove other weather app related file

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using OXL_Assessment2.Data;
-using OXL_Assessment2.Data.Entities;
 using OXL_Assessment2.Interface;
 using OXL_Assessment2.Interface.IServices;
 using OXL_Assessment2.Src.Repositories;
@@ -32,30 +31,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast = Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
-
-// map the controllers
 app.MapControllers();
+
 app.Run();
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
