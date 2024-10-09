ad5caafc1815dfd054e19683da49ab9b3ebc7a0d
chi xu <xuchi007@hotmail.com>
2024-10-09 13:16:09 +1300

    OXLA-17 constants message and code

---
+++ Src/Constants/ServiceCode.cs
@@ -0,0 +1,17 @@
+/// <summary>
+/// code list of http response
+/// </summary>
+/// <remarks>
+/// each code maps service operation
+/// </remarks>
+/// <author>
+/// Chi Xu (Peter) -- 09/10/2024
+/// </author>
+namespace OXL_Assessment2.Src.Constants;
+
+public enum ServiceCode
+{
+  // 01xx category relavant
+  GetAllCategoriesSuccessfully = 100,
+  NoCategoriesFound = 101
+}
