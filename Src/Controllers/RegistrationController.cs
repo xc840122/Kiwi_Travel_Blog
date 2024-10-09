4731fd9eb30706ed984dc2ae08e0b71afab2dfd8
chi xu <xuchi007@hotmail.com>
2024-10-09 20:37:46 +1300

    config Identity fx

---
+++ Src/Controllers/RegistrationController.cs
@@ -0,0 +1,20 @@
+/// <summary>
+/// registration api
+/// </summary>
+/// <remarks>
+/// registration feature for normal user
+/// </remarks>
+/// <author>
+/// Chi Xu (Peter) -- 09/10/2024
+/// </author>
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OXL_Assessment2.Src.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+    }
+}
