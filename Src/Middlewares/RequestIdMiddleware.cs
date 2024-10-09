c3e1ec8e9e6ab3bf5a56f73150b2961504c110a1
chi xu <xuchi007@hotmail.com>
2024-10-09 17:36:50 +1300

    insert request id into http request header, not items

--- Src/Middlewares/RequestIdMiddleware.cs
+++ Src/Middlewares/RequestIdMiddleware.cs
@@ -23,12 +23,16 @@ public class RequestIdMiddleware
 
   public async Task InvokeAsync(HttpContext httpContext)
   {
-    // generate unique request ID
-    var requestId = Guid.NewGuid().ToString();
+    // generate unique request ID, add to item (limited scope for this request)
+    // var requestId = Guid.NewGuid().ToString();
+    // httpContext.Items["RequestId"] = requestId;
 
-    // add id to request items
-    httpContext.Items["RequestId"] = requestId;
-
+    // Add to header Check if Request-ID is provided by the client (through more services)
+    if (!httpContext.Request.Headers.TryGetValue("X-Request-ID", out var requestId))
+    {
+      // generate one id if not exist in request header
+      requestId = Guid.NewGuid().ToString();
+    }
     // add id to response header
     httpContext.Response.Headers.Append("X-Request-ID", requestId);
 
