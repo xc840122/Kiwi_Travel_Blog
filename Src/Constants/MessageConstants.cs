4000bf963c7775ca7f1f172512b37803a5325ed3
chi xu <xuchi007@hotmail.com>
2024-10-10 15:02:28 +1300

    OXLA-18 Add login api

--- Src/Constants/MessageConstants.cs
+++ Src/Constants/MessageConstants.cs
@@ -16,4 +16,6 @@ public class MessageConstants
   public const string GetCategoriesSuccessfully = "Get categories successfully";
   public const string NotFoundData = "Not found data";
   public const string RegisterSuccessfully = "Register Successfully";
+  public const string UserNotExist = "User not exist";
+  public const string PasswordNotCorrect = "Password not correct";
 }
