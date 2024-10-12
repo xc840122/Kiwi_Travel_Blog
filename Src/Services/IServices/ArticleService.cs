0456ebbda3fa2fd12d026cc3fd426958751576e4
chi xu <xuchi007@hotmail.com>
2024-10-12 22:52:10 +1300

    inject article service and repository

--- Src/Services/IServices/ArticleService.cs
+++ Src/Services/IServices/ArticleService.cs
@@ -1,7 +1,28 @@
 using System;
+using OXL_Assessment2.Src.Dtos;
+using OXL_Assessment2.Src.Repositories.IRepositories;
 
 namespace OXL_Assessment2.Src.Services.IServices;
-
-public class ArticleService
+/// <summary>
+/// service of article
+/// </summary>
+public class ArticleService : IArticleService
 {
+  private readonly IArticleRepository _articleRepository;
+  private readonly ILogger<ArticleService> _logger;
+
+  public ArticleService(IArticleRepository articleRepository, ILogger<ArticleService> logger)
+  {
+    _articleRepository = articleRepository;
+    _logger = logger;
+  }
+  /// <summary>
+  /// service method: get articles by category id
+  /// </summary>
+  /// <param name="CategoryId"></param>
+  /// <returns>List<ArticleDto></returns>
+  public Task<List<ArticleDto>> GetArticlesByCategoryId(long CategoryId)
+  {
+    throw new NotImplementedException();
+  }
 }
