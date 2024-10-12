c736518eab27973d60319882ddf044dc12c4767f
chi xu <xuchi007@hotmail.com>
2024-10-12 21:13:35 +1300

    rebuild database

---
+++ Src/Data/Migrations/App/20241012081200_InitialDb.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OXL_Assessment2.Src.Data.Migrations.App
+{
+    /// <inheritdoc />
+    public partial class InitialDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "T_Categories",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Position = table.Column<int>(type: "int", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "T_Articles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LikeNums = table.Column<long>(type: "bigint", nullable: false),
+                    FavoriteNums = table.Column<long>(type: "bigint", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryId = table.Column<long>(type: "bigint", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_Articles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_T_Articles_T_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "T_Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "T_Comments",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Review = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Reviewer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ArticleId = table.Column<long>(type: "bigint", nullable: false),
+                    LikeNum = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_T_Comments_T_Articles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "T_Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "T_Images",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ArticleId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_Images", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_T_Images_T_Articles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "T_Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_Articles_CategoryId",
+                table: "T_Articles",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_Comments_ArticleId",
+                table: "T_Comments",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_Images_ArticleId",
+                table: "T_Images",
+                column: "ArticleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "T_Comments");
+
+            migrationBuilder.DropTable(
+                name: "T_Images");
+
+            migrationBuilder.DropTable(
+                name: "T_Articles");
+
+            migrationBuilder.DropTable(
+                name: "T_Categories");
+        }
+    }
+}
