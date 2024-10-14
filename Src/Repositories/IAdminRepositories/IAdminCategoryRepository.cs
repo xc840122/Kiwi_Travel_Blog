087989618292ddf4b5b56e64e6a6a9ce93ac35cd
Chi Xu <xuchi007@hotmail.com>
2024-10-14 21:52:47 +1300

    OXLA-36 Repository of Add category

--- Src/Repositories/IAdminRepositories/IAdminCategoryRepository.cs
+++ Src/Repositories/IAdminRepositories/IAdminCategoryRepository.cs
@@ -1,14 +1,15 @@
 using System;
+using Kiwi_Travel_Blog.Data.Entities;
 
 namespace Kiwi_Travel_Blog.Src.Repositories.IAdminRepositories;
 /// <summary>
-/// Admin interface of article repository
+/// Admin interface of category repository
 /// </summary>
-public interface IAdminArticleRepository
+public interface IAdminCategoryRepository
 {
   /// <summary>
   /// Insert category into database
   /// </summary>
   /// <returns></returns>
-  public Task InsertCategory();
+  public Task InsertCategory(Category category);
 }
