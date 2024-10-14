c52ff15c5ceeb08f8c49a701423f0152485b7792
Chi Xu <xuchi007@hotmail.com>
2024-10-14 14:37:53 +1300

    remove auto-generate text about conflict

--- Src/Configuration/ServiceConfiguration.cs
+++ Src/Configuration/ServiceConfiguration.cs
@@ -1,10 +1,7 @@
 using System;
-<<<<<<< HEAD
 using Kiwi_Travel_Blog.Interface;
 using Kiwi_Travel_Blog.Src.Services;
 using Kiwi_Travel_Blog.Src.Services.IServices;
-=======
->>>>>>> OXLA-34
 
 namespace Kiwi_Travel_Blog.Src.Configuration;
 
