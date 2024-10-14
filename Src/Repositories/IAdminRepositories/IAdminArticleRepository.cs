22dfda7e471138daecee293dabf8c7148608e8b0
Chi Xu <xuchi007@hotmail.com>
2024-10-14 20:59:42 +1300

    admin interface of article repository

--- Src/Repositories/IAdminRepositories/IAdminArticleRepository.cs
+++ Src/Repositories/IAdminRepositories/IAdminArticleRepository.cs
@@ -1,8 +1,14 @@
 using System;
 
 namespace Kiwi_Travel_Blog.Src.Repositories.IAdminRepositories;
-
+/// <summary>
+/// Admin interface of article repository
+/// </summary>
 public interface IAdminArticleRepository
 {
-
+  /// <summary>
+  /// Insert category into database
+  /// </summary>
+  /// <returns></returns>
+  public Task InsertCategory();
 }
